a8e2b413db13524ac0f6b07b266afe69a4b51ea5
andreilupu <andrei.lupu@pixelgrade.com>
2016-10-10 14:40:24 +0300

    add qunit tests

---
+++ tests/qunit/editor/js/tinymce_loader.js
@@ -0,0 +1,7 @@
+// Edited for WordPres
+document.write('<script src="../../../src/wp-includes/js/tinymce/tinymce.js"></script>');
+
+var wpPlugins = 'charmap colorpicker hr lists media paste tabfocus textcolor ' +
+			'fullscreen wordpress wpautoresize wpeditimage wpgallery wplink wpdialogs wpview';
+
+getUserSetting = setUserSetting = function() {}
